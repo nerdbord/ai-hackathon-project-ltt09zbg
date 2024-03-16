d94bfbab3512dae2155b42a3963e243117484906
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-03-16 15:59:23 +0100

    feat: pass data to db - get user data from his telegram acc

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,43 @@
-import express, { Request, Response } from "express";
-import GptClient from "./gpt/gpt.service";
+import express, { Request, Response } from 'express';
+import GptClient from './gpt/gpt.service';
+import { Context, Middleware } from 'telegraf';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  require("dotenv").config();
-  const { Telegraf } = require("telegraf");
+  require('dotenv').config();
+  const { Telegraf } = require('telegraf');
 
-  const telegramApiKey = process.env.TELEGRAM_API_KEY || "TELEGRAM_API_KEY";
+  const telegramApiKey = process.env.TELEGRAM_API_KEY || 'TELEGRAM_API_KEY';
   // Inicjalizacja bota Telegram
   const bot = new Telegraf(telegramApiKey);
   const gptClient = new GptClient();
+
+  bot.start((ctx: Context) => {
+    // Pobierz imię użytkownika
+    const chatId = ctx.chat?.id; // tego uzywamy
+    const firstName = ctx.from?.first_name;
+    const userId = ctx.from?.id;
+    const lastName = ctx.from?.last_name;
+    const languageCode = ctx.from?.language_code;
+
+    // wrzucoic do prismy
+
+    // Wyślij wiadomość powitalną
+    ctx.reply(
+      `Witaj ${firstName}! Dziękujemy, że dołączyłeś do naszego czatu.`
+    );
+
+    ctx.reply(`${chatId} ${firstName} ${userId} ${lastName} ${languageCode}`);
+  });
   // Obsługa wiadomości od użytkowników
-  bot.on("text", async (ctx: any) => {
+  bot.on('text', async (ctx: any) => {
     const userMessage = ctx.message.text;
 
     // Wywołaj ChatGPT, aby uzyskać odpowiedź na wiadomość użytkownika
@@ -31,13 +50,13 @@ app.listen(port, () => {
       // Odpowiedź użytkownikowi
       ctx.reply(botReply);
     } catch (error) {
-      console.error("Błąd podczas komunikacji z OpenAI:", error);
-      ctx.reply("Przepraszam, coś poszło nie tak.");
+      console.error('Błąd podczas komunikacji z OpenAI:', error);
+      ctx.reply('Przepraszam, coś poszło nie tak.');
     }
   });
 
   // Start bota
   bot.launch();
 
-  console.log("Bot uruchomiony. Czekam na wiadomości...");
+  console.log('Bot uruchomiony. Czekam na wiadomości...');
 });
